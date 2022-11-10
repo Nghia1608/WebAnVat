b130595e20f62ad7e99a7465f5182f7dab096ee9
Nghia1608 <badaosv95@gmail.com>
2022-11-10 10:50:19 +0700

    tong tien gio hang

---
+++ src/app/models/ProductsOrders.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
+
+
+
+const ProductsOrder = new Schema({
+    username :{type :String ,maxLength : 50,require:true,unique:true},
+    password :{type :String,maxLength:250,require:true},
+    quyen :{type :String,maxLength:10,require:true},
+    email :{type :String,maxLength:254,require:true},
+    hoTen :{type :String,maxLength:50,require:true},
+    sdt :{type :String,maxLength:12,require:true},
+
+  },{
+    timestamps : true,
+  });
+
+module.exports = mongoose.model('ProductsOrders',ProductsOrder);
